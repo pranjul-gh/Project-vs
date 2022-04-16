c4c9df0a9b150a71f4382d5cab9ca028d027107d
Pranjul <pranjulmaurya.23@gmail.com>
2022-04-16 12:26:30 +0530

    to v1.1

--- sketchpyy.py
+++ sketchpyy.py
@@ -5,8 +5,15 @@ def func(x,y):
     x=x**y
     print(x,"multiplied with",y" is", z)
     print(x,"raised to poewer",y" is:",x)
-func()
 def disp():
     for j in range(1,10):
         for i in range(j):
             print(i," ",j)
+def another():
+    lst = list(12,20,30,40,)
+    lst[0] = 10
+    print(lst)
+
+another()
+disp()
+func()
